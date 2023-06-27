9f7d3d5dcff05a6ecbf3b4ab4081baee8cee3a57
HuseinliAli <ali.guseynoov@gmail.com>
2023-06-27 20:16:28 +0400

    array states

--- src/States/UseStateComponent.jsx
+++ src/States/UseStateComponent.jsx
@@ -2,11 +2,21 @@ import React, { useState } from "react";
 
 const UseStateComponent = () => {
   const [name, setName] = useState("ali");
-
+  const [friends, setFriends] = useState(["riko", "amuko", "makolo"]);
   return (
     <div>
       <h1>{name}</h1>
       <button onClick={() => setName("huseyn")}>change name</button>
+      <br />
+      <br />
+      <br />
+      <h2>{name}'s Friends</h2>
+      {friends.map((friend, index) => (
+        <p key={index}>{friend}</p>
+      ))}
+      <button onClick={() => setFriends([...friends, "nikolo"])}>
+        add friend
+      </button>
     </div>
   );
 };
