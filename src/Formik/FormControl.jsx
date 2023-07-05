d0d844e6ece45f556e5fb199a783edb2040b8f67
HuseinliAli <ali.guseynoov@gmail.com>
2023-07-05 23:25:37 +0400

    handleChange & handleSubmit

--- src/Formik/FormControl.jsx
+++ src/Formik/FormControl.jsx
@@ -1,38 +1,49 @@
-import React from 'react'
-import { Formik,Field,Form} from "formik";
+import React from "react";
+import { Formik, Field, Form } from "formik";
 const FormControl = () => {
   return (
     <div>
-          <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 500));
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field id="firstName" name="firstName" placeholder="Jane" />
-
-        <label htmlFor="lastName">Last Name</label>
-        <Field id="lastName" name="lastName" placeholder="Doe" />
-
-        <label htmlFor="email">Email</label>
-        <Field
-          id="email"
-          name="email"
-          placeholder="jane@acme.com"
-          type="email"
-        />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        {({ handleSubmit, handleChange }) => (
+          <Form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              onChange={handleChange}
+              id="firstName"
+              name="firstName"
+              placeholder="Jane"
+            />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              onChange={handleChange}
+              id="lastName"
+              name="lastName"
+              placeholder="Doe"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default FormControl
+export default FormControl;
