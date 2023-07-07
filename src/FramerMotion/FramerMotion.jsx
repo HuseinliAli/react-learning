6b934d67d9d918c621918439fc33c6faf6eb2f83
HuseinliAli <ali.guseynoov@gmail.com>
2023-07-07 22:44:34 +0400

    css error handled

--- src/FramerMotion/FramerMotion.jsx
+++ src/FramerMotion/FramerMotion.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "../logo192.png";
-import "./framer-motion.module.css";
+import "./framer.css";
 import { motion } from "framer-motion";
 const FramerMotion = () => {
   return (
-    <div className="animation-center">
+    <motion.div className="animation-center">
       <motion.div
         initial={{ opacity: 1 }}
         animate={{ opacity: 0 }}
@@ -17,12 +17,12 @@ const FramerMotion = () => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 5 }}
+        transition={{ duration: 5, delay: 2 }}
         className="framer-studio-div"
       >
         <h2>framer studio</h2>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
