da60352c66ade804a8fd1470fa8c921a68079e03
HuseinliAli <ali.guseynoov@gmail.com>
2023-07-11 13:38:14 +0400

    context provider

--- src/ContextApi/Components/Application.jsx
+++ src/ContextApi/Components/Application.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ThemeContext from "../context/ThemeContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import ChildComponent from "./ChildComponent";
 const Application = () => {
   return (
-    <ThemeContext.Provider value="dark">
+    <ThemeProvider>
       <ChildComponent />
-    </ThemeContext.Provider>
+    </ThemeProvider>
   );
 };
 
--- src/ContextApi/Components/ChildComponent.jsx
+++ src/ContextApi/Components/ChildComponent.jsx
@@ -1,8 +1,18 @@
 import React, { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
 const ChildComponent = () => {
-  const data = useContext(ThemeContext);
-  return <button className={`btn btn-${data}`}>Button</button>;
+  const { theme, setTheme } = useContext(ThemeContext);
+  return (
+    <div className={`bg-${theme} text-${theme === "light" ? "dark" : "light"}`}>
+      AKSLKDLSDLKASLKD{" "}
+      <button
+        className={`btn btn-${theme === "light" ? "dark" : "light"}`}
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        change theme
+      </button>
+    </div>
+  );
 };
 
 export default ChildComponent;
